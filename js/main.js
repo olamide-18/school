4dc93c5a76944a23408fa7cefda649e8955e0b74
olamide <emmanuelegbebi2004@gmail.com>
2023-04-25 19:49:00 -0800

    initial upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let contact = document.getElementById("contact-section");
+
+function openContactSection(){
+    contact.style.display = "block";
+}
+
+function closeContactSection(){
+    contact.style.display = "none";
+}
